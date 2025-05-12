84ad790aac222a8d3bc91ccb19fc12c43830ab7d
cuhp293 <phuchong292003@gmail.com>
2025-05-12 17:45:08 +0700

    Flutter: WIDGETS - scaffold, container, text, icon, app bar + basic CSS

--- Flutter/flutter_beginner_course/lib/main.dart
+++ Flutter/flutter_beginner_course/lib/main.dart
@@ -9,9 +9,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false, // hide Debug Banner in phone (demo)
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        appBar: AppBar(
+          title: Text("My App Bar"),
+          backgroundColor: Colors.green,
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.logout)),
+          ],
+        ),
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.green,
+              // curve the corners a bit
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(25),
+          /* child: Text(
+              "dthp",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ), */
+            child: Icon(
+              Icons.favorite,
+              color: Colors.white,
+              size: 64,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
